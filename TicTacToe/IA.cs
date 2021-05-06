c005709a60d8728862c5f699bf103e61e65db440
LuisLara-UH <luis.lara.uh42@gmail.com>
2021-05-05 22:18:40 -0400

    Add project files.

---
+++ TicTacToe/IA.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class IA
+    {
+        public IA()
+        {
+            States = new List<TableState>() 
+            { 
+                TableState.Cross, TableState.Neutral, TableState.Zero
+            };
+            InitializeTables();
+            InitializeOptimalMoves();
+        }
+
+        public List<Table> Tables { get; set; }
+        public List<TableState> States { get; set; }
+
+        public void InitializeTables()
+        {
+            Table initTable = new Table();
+            CalculateTables(0, initTable);
+        }
+
+        public void CalculateTables(int pos, Table table)
+        {
+            if (pos == 9)
+            {
+                Tables.Add(new Table(table));
+                return;
+            }
+
+            foreach (var state in States)
+            {
+                table.Mark(pos, state);
+                CalculateTables(pos + 1, table);
+            }
+        }
+
+        public void InitializeOptimalMoves()
+        { 
+            
+        }
+
+        private bool FindIAOptimalMove(Table table)
+        {
+            foreach (var move in table.PossibleMoves(TableState.Zero))
+            {
+                if (IsOptimalMove(move))
+                { 
+                    
+                }
+            }
+            return false;
+        }
+
+        private bool IsOptimalMove(Table table)
+        {
+            foreach (var move in table.PossibleMoves(TableState.Cross))
+            { 
+            
+            }
+            return true;
+        }
+    }
+
+    public class Table
+    {
+        public Table()
+        {
+            States = new TableState[CantSquares];
+            for (int i = 0; i < CantSquares; ++i)
+                States[i] = TableState.Neutral;
+        }
+
+        public Table(Table table)
+        {
+            States = new TableState[CantSquares];
+            Array.Copy(table.States, States, CantSquares);
+        }
+
+        public void Mark(TableSquare square, TableState state)
+        {
+            States[square.SquareNumber] = state;
+        }
+
+        public void Mark(int pos, TableState state)
+        {
+            States[pos] = state;
+        }
+
+        public List<Table> PossibleMoves(TableState state)
+        {
+            List<Table> possibleMoves = new List<Table>();
+            for (int i = 0; i < CantSquares; ++i)
+                if (States[i] == TableState.Neutral)
+                {
+                    States[i] = state;
+                    possibleMoves.Add(new Table(this));
+                    States[i] = TableState.Neutral;
+                }
+            return possibleMoves;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Table)
+            {
+                Table table = (obj as Table);
+
+                for (int i = 0; i < CantSquares; ++i)
+                    if (States[i] != table.States[i])
+                        return false;
+                return true;
+            }
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = (int)States[0];
+            for (int i = 1; i < CantSquares; ++i)
+                hashCode = (hashCode * 10) + (int)States[i];
+
+            return hashCode;
+        }
+
+        public TableState[] States { get; private set; }
+        public int CantSquares => 9;
+
+    }
+
+    public class TableSquare
+    {
+        private int _row;
+        private int _column;
+        public TableSquare(int row, int column)
+        {
+            _row = row;
+            _column = column;
+        }
+
+        public int SquareNumber => (_row * 3) + _column;
+    }
+
+    public enum TableState
+    { 
+        Cross, Zero, Neutral
+    }
+}
---
+++ TicTacToe/MainForm.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace TicTacToe
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.table = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.table)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // table
+            // 
+            this.table.Location = new System.Drawing.Point(12, 12);
+            this.table.Name = "table";
+            this.table.Size = new System.Drawing.Size(309, 204);
+            this.table.TabIndex = 0;
+            this.table.TabStop = false;
+            this.table.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.table);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.table)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.PictureBox table;
+    }
+}
+
